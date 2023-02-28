3df934ef7f177bf96da29e8c3aba5bed9534f74c
kostaspoursanidis <56089469+kostaspoursanidis@users.noreply.github.com>
2023-02-28 22:29:38 +0200

    Final upload of Thesis App

---
+++ main/java/com/model/Fruit.java
@@ -0,0 +1,49 @@
+package com.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Fruit {
+
+    @Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	private Long id;
+
+    private Long prod_id;
+    private String type;
+
+    public Fruit(Long prod_id,String type){
+        this.prod_id = prod_id;
+        this.type = type;
+    }
+
+    public Fruit(){};
+
+    public String getType(){
+        return type;
+    }
+
+    public Long getProd_id(){
+        return prod_id;
+    }
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setProd_id(Long prod_id) {
+		this.prod_id = prod_id;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+    
+    
+}
---
+++ main/java/com/services/RatingService.java
@@ -0,0 +1,35 @@
+package com.services;
+
+import java.util.List;
+
+import com.model.Rating;
+import com.repos.RatingRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RatingService {
+
+    @Autowired
+    private RatingRepo ratingRepo;
+
+    public List<Rating> getAllUserRating(Long id){
+
+        List<Rating> ratings = ratingRepo.getAllUserRatings(id);
+
+        return ratings;
+
+    }
+
+    public void save(Rating newRating) {
+
+        ratingRepo.save(newRating);
+    }
+
+    public Rating getSpecificUserRating(Long id,Long auth_id){
+        return ratingRepo.getSpecificUserRating(id, auth_id);
+    }
+
+    
+}
